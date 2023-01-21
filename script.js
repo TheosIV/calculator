55450953a4a539891892ba9364da62658ab56d45
Ilyas Hajib <91851958+TheosIV@users.noreply.github.com>
2023-01-21 13:23:29 +0100

    Create simple calculator with basic operations
     Create 4 functions, each of them returns the result of one basic
     operation (add, subtract, multiply, divide). Each one of these function
     round the result to 3 decimal places, that will fix the overflow of
     numbers outside the display when the fractional part of the result is
     too long.
    
     Create a function called operate that takes an array, like this one
     ([number, operator, number]), as an argument. This function check the
     operator that is present in the array and according to that, it calls
     one of the above fucntions on the array and return the result. If the
     result string is is larger that 10 characters, the function transform
     it to scientific notation.
    
     Create a function called displayNumbers that takes a number as an
     argument and display it.
    
     Create a function "setOperator" that takes an operator as an argument,
     then add the displayed numbers, and the operator to an array called
     "operationArray".
    
     Create a function "setDecimalPoint" that adds decimal point to a number
     only one time.
    
     Create a function "finalResult" that takes the operationArray as an
     argument, and call the fucntion "operate" on it only if its length = 3.
    
     Add eventListener to all the numbers, call the function
     "displayNumbers" with the clicked number as an argument, and then
     remove the "active" class from any operator.
    
     Add eventListener to the decimal point, that call the fucntion
     "setDecimalPoint", and then remove the "active" class from any
     operator.
    
     Add eventListener to all operators, that toggles the active class for
     only the clicked operator, one at a time. then check if the
     "operationArray" length = 4, to call the "displayNumbers" and "operate"
     as its argument on the 3 first element in the array, then replace the
     array 0 index with the result and remove the first and the second
     indexes.
    
     Add eventListener to the clear button, that removes the first character
     from the right in the display.
    
     Add eventListener to the AC button, that reset the display and also
     everything.
    
     Add eventListener to the "equal" button, that add the "active" calss to
     it, add the displayed number to the "operationArray", and then call the
     "finalResult" fucntion, and reset "operationArray".

--- script.js
+++ script.js
@@ -1,129 +1,218 @@
-function display() {
-    buttons.forEach(button => {
-        let clicked = button.textContent;
-        
-        button.addEventListener('click', () => {
-            if (operationDisplay.textContent === "" && button.id === 'operator') {
-                operationDisplay.innerText += "";
-            }else if (button.id === 'operator' && operators.some(item => operationDisplay.textContent.includes(item))) {
-                operationDisplay.innerText = clicked;
-            }else if (button.id === 'operator' && !operators.some(item => operationDisplay.textContent.includes(item))) {
-                operation.push(operationDisplay.textContent);
-                resultDisplay.innerText += operationDisplay.textContent;
-                operationDisplay.innerText = clicked;
-            }else if (clicked === '0' && operationDisplay.textContent === '/') {
-                dividingByZero = true
-                operation.push(operationDisplay.textContent);
-                resultDisplay.innerText += operationDisplay.textContent;
-                alert("Math Error")
-                operationDisplay.innerText = '0';
-            }else if (button.id === 'number' && dividingByZero) {
-                dividingByZero = false;
-                operationDisplay.innerText = clicked;
-            }else if (button.id === 'number' && !operators.some(item => operationDisplay.textContent.includes(item)) && previouslyClicked !== '=') {
-                operationDisplay.innerText += clicked;
-            }else if (button.id === 'number' && operators.some(item => operationDisplay.textContent.includes(item)) && previouslyClicked !== '=') {
-                console.log('seb')
-                operation.push(operationDisplay.textContent);
-                resultDisplay.innerText += operationDisplay.textContent;
-                operationDisplay.innerText = clicked;
-            }else if (button.id === 'number' && previouslyClicked === '=') {
-                resultDisplay.innerText = "";
-                operationDisplay.innerText = clicked;
-                previouslyClicked = null;
-            }else if (button.id === '.' && !operationDisplay.textContent.includes('.') && !operators.some(item => operationDisplay.textContent.includes(item))) {
-                operationDisplay.innerText += clicked;
-            }else if (button.id === '.' && operationDisplay.textContent.includes('.')) {
-                operationDisplay.innerText += "";
-            }else if (button.id === 'equal') {
+function add(x, y) {
+    return Math.round((+x + +y)*1000)/1000;
+};
+
+function subtract(x, y) {
+    return Math.round((+x - +y)*1000)/1000;
+};
+
+function multiply(x, y) {
+    return Math.round((+x * +y)*1000)/1000;
+};
+
+function divide(x, y) {
+    return Math.round((+x / +y)*1000)/1000;
+};
+
+// operate function actually evaluate the entire operation and return mathematically the right result, which is not what the project is asking for
+/*function operate(operationArray) {
+
+    if (operationArray.includes('*') || operationArray.includes('/')) {
+        for (let i = 0; i < operationArray.length - 1; i++) {
+            if (operationArray[i] == '*') {
+                operationArray[i-1] = multiply(operationArray[i-1], operationArray[i+1]);
+                operationArray.splice(i, 2);
+                i -= 2;
                 
-                if (operators.some(item => operationDisplay.textContent.includes(item)) || dividingByZero) {
-                    console.log('mm')
-                }else {
-                    operation.push(operationDisplay.textContent);
-                    console.log(operation)
-                    operationDisplay.innerText = "";
-                    resultDisplay.innerText = operate(operation);
-                    ans = resultDisplay.textContent;
-                    operation = [];
-                    previouslyClicked = '=';
-                }
-            }else if (button.id == 'ans') {
-                resultDisplay.innerText = "";
-                operationDisplay.innerText = ans;
-                previouslyClicked = null;
-            }
-            
-        });
-    });
-
-    allClear.addEventListener('click', () => {
-        operationDisplay.innerText = "";
-        resultDisplay.innerText = "";
-        operation = [];
-    });
-    
-    clear.addEventListener('click', () => {
-        operationDisplay.innerText = operationDisplay.textContent.slice(0, operationDisplay.textContent.length - 1); 
-    });
-};
-
-
-function operate(array) {
-    if (array.includes('x')|| array.includes('/')) {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] === 'x') {
-                array[i-1] = +array[i-1] * +array[i+1]; 
-                array.splice(i, 2);
-                i -= 2;
-
-            }else if (array[i] === '/') {
-                array[i-1] = +array[i-1] / +array[i+1]; 
-                array.splice(i, 2);
+            }else if (operationArray[i] == '/') {
+                operationArray[i-1] = divide(operationArray[i-1], operationArray[i+1]);
+                operationArray.splice(i, 2);
                 i -= 2;
 
             }else {
                 continue;
+
             };
         };
+    };
+
+    if (operationArray.includes('-') || operationArray.includes('+')) {
+        for (let i = 0; i < operationArray.length - 1; i++) {
+            if (operationArray[i] == '-') {
+                operationArray[i-1] = subtract(operationArray[i-1], operationArray[i+1]);
+                operationArray.splice(i, 2);
+                i -= 2;
+
+            }else if (operationArray[i] == '+') {
+                operationArray[i-1] = add(operationArray[i-1], operationArray[i+1]);
+                operationArray.splice(i, 2);
+                i -= 2;
+
+            }else {
+                continue;
+
+            };
+        };
+    };
+
+    return operationArray;
+};*/
+
+// Each pair of numbers being evaluated at a time, operationArray = [number, operator, number]
+function operate(operationArray) {
+    if (operationArray.includes('*')) {
+        if (`${multiply(operationArray[0], operationArray[2])}`.length > 10) {
+
+            return multiply(operationArray[0], operationArray[2]).toExponential(4);
+        }else {
+            return multiply(operationArray[0], operationArray[2]);
+        };
+
+    };
+
+    if (operationArray.includes('/')) {
+        if (`${divide(operationArray[0], operationArray[2])}`.length > 10) {
+
+            return divide(operationArray[0], operationArray[2]).toExponential(4);
+        }else {
+            return divide(operationArray[0], operationArray[2]);
+        };
+
     };
     
-    if (array.includes('-')) {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] === '-') {
-                array[i-1] = +array[i-1] - +array[i+1]; 
-                array.splice(i, 2);
-                i -= 2;
-            }else {
-                continue;
-            };
+    if (operationArray.includes('+')) {
+        if (`${add(operationArray[0], operationArray[2])}`.length > 10) {
+
+            return add(operationArray[0], operationArray[2]).toExponential(4);
+        }else {
+            return add(operationArray[0], operationArray[2]);
         };
     };
-    if (array.includes('+')) {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] === '+') {
-                array[i-1] = +array[i-1] + +array[i+1]; 
-                array.splice(i, 2);
-                i -= 2;
-            }else {
-                continue;
-            };
+
+    if (operationArray.includes('-')) {
+        if (`${subtract(operationArray[0], operationArray[2])}`.length > 10) {
+
+            return subtract(operationArray[0], operationArray[2]).toExponential(4);
+        }else {
+            return subtract(operationArray[0], operationArray[2]);
         };
+
     };
-    return array;
+
 };
 
+function displayNumbers(number) {
+    const active = document.querySelector('.active');
+    if (display.textContent == '0') {
+        display.innerText = "";
+
+    };
+    
+    if (active || equal.classList.value.includes('active')) {
+        display.innerText = number;
+
+    }else {
+        if (display.textContent.length == 10) {
+            display.innerText += "";
+        }else {
+            display.innerText += number;
+        }
+
+    };
+
+};
+
+function setOperator(operator) {
+    operationArray.push(display.textContent, operator);
+};
+
+function setDecimalPoint() {
+    const active = document.querySelector('.active');
+    if (active) {
+        if (!display.textContent.includes('.')) {
+            display.innerText = '0.';
+        };
+    }else {
+        if (!display.textContent.includes('.')) {
+            display.innerText += '.';
+        };
+    };
+};
+
+function finalResult(operationArray) {
+    if (operationArray.length === 3) {
+        display.innerText = operate(operationArray);
+    };
+};
 
 const buttons = document.querySelectorAll('.button');
-const operationDisplay = document.querySelector('#operation');
-const resultDisplay = document.querySelector('#result')
-const decimalPoint = document.querySelector('#decimal-point');
-const allClear = document.querySelector('#all-clear');
+const numbers = document.querySelectorAll('#number');
+const operators = document.querySelectorAll('#operator');
+const display = document.querySelector('#display');
 const clear = document.querySelector('#clear');
-const operators = ['/', '+', '-', 'x'];
-let operation = [];
-let ans = null;
-let previouslyClicked = null;
-let dividingByZero = false;
+const allClear = document.querySelector('#all-clear');
+const decimalPoint = document.querySelector('#decimal');
+const equal = document.querySelector('#equal');
+let operationArray = [];
 
-display();
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        displayNumbers(number.textContent);
+        operators.forEach(operator => {
+            operator.classList.remove('active');
+        });
+        equal.classList.remove('active');
+    });
+    
+});
+
+decimalPoint.addEventListener('click', () => {
+    setDecimalPoint();
+    operators.forEach(operator => {
+        operator.classList.remove('active');
+    });
+});
+
+operators.forEach(operator => {
+    operator.addEventListener('click', (e) => {
+        const active = document.querySelector('.active');
+        if (active) {
+            active.classList.remove('active');
+        };
+        
+        equal.classList.remove('active');
+        e.target.classList.add('active');
+        setOperator(e.target.textContent);
+
+        if (operationArray.length == 4) {
+            displayNumbers(operate([operationArray[0], operationArray[1], operationArray[2]]));
+            operationArray[0] = display.textContent;
+            operationArray.splice(1, 2);
+        };
+
+    });
+    
+});
+
+clear.addEventListener('click', () => {
+    if (display.textContent.length == 1) {
+        display.innerText = "0";
+    }else {
+        display.innerText = display.textContent.slice(0, display.textContent.length-1);
+    };
+});
+
+allClear.addEventListener('click', () => {
+    display.innerText = "0";
+    operationArray = [];
+    operators.forEach(operator => {
+        operator.classList.remove('active');
+    });
+} );
+
+equal.addEventListener('click', () => {
+    equal.classList.add('active');
+    operationArray.push(display.textContent);
+    finalResult(operationArray);
+    operationArray = [];
+});
